84ba35675acbfaa95c7db9ac6915077f04bcab2c
AyuDewiH <harumidewiayu@gmail.com>
2025-05-27 14:34:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,13 @@ st.write(
     "ngoding seru bersama ayu dewi"
 )
 st.image("1dc14417c4032a266dbacc8fe2dd05b0.jpg", width=200)
+
+st.tittle("Aplikasi Sederhana")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah Angka
+
+if (angka % 2) == 0:
+    st.write(f"(angka) adalah Bilangan Genap")
+else :
+    st.write(f"(angka) adalah Bilangan Ganjil") -!
+
